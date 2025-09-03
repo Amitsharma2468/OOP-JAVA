7a20bced8062f6b77b48c891ec0b06c4d9770831
Amit Kumar <amitkmrsharma292@gmail.com>
2025-09-03 17:17:46 +0600

    static method

---
+++ static_method/StaticMethod.java
@@ -0,0 +1,12 @@
+package static_method;
+
+public class StaticMethod {
+
+    void display1() {
+        System.out.println("Non Static Method");
+    }
+
+    static void display2() {
+        System.out.println("Static Method");
+    }
+}
